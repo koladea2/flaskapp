7d82d319d27f09f0f7b196e23404905ff105be62
Kolade Adegbaye <koladea@bu.edu>
2021-07-14 22:00:55 +0000

    day3's work

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import time, random, threading
+from turbo_flask import Turbo
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm
 from flask_sqlalchemy import SQLAlchemy
+from audio import printWAV
 
 app = Flask(__name__)
 
@@ -9,6 +12,10 @@ app.config['SECRET_KEY'] = '9eab7f836e62c6942456cf37136816e2'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+interval=10
+FILE_NAME = "speaking.wav"
+turbo = Turbo(app)
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -27,7 +34,47 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', subtitle='About Page', text='This is the about page')
-   
+ 
+@app.route("/captions")
+def captions():
+    TITLE = "speaking_file"
+    return render_template('captions.html', songName=TITLE, file=FILE_NAME)
+
+@app.before_first_request
+def before_first_request():
+    #resetting time stamp file to 0
+    file = open("pos.txt","w") 
+    file.write(str(0))
+    file.close()
+
+    #starting thread that will time updates
+    threading.Thread(target=update_captions, daemon=True).start()
+
+@app.context_processor
+def inject_load():
+    # getting previous time stamp
+    file = open("pos.txt","r")
+    pos = int(file.read())
+    file.close()
+
+    # writing next time stamp
+    file = open("pos.txt","w")
+    file.write(str(pos+interval))
+    file.close()
+
+    #returning captions
+    return {'caption':printWAV(FILE_NAME, pos=pos, clip=interval)}
+
+def update_captions():
+    with app.app_context():
+        while True:
+            # timing thread waiting for the interval
+            time.sleep(interval)
+
+            # forcefully updating captionsPane with caption
+            turbo.push(turbo.replace(render_template('captionsPane.html'), 'load'))
+
+            
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()  
